7558c0e14896654129f094c81890fe10b52cba70
UDAY B <udaybhati134@gmail.com>
2024-04-21 20:51:07 +0530

    changed datbase

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose= require('mongoose');
-const mongoURI= "mongodb+srv://udaybhati134:cfyTTS3aSHihb3Ww@cluster0.8h7vtvg.mongodb.net/NoteInfinity?retryWrites=true&w=majority" || "mongodb://127.0.0.1:27017/noteinfinity"
+const mongoURI= "mongodb+srv://udaybhati134:cfyTTS3aSHihb3Ww@cluster0.8h7vtvg.mongodb.net/NoteInfinix?retryWrites=true&w=majority"
 
 const connectm=()=>{
     mongoose.connect(mongoURI,).then(()=>console.log('hello')).catch((err)=>{console.error(err);});
